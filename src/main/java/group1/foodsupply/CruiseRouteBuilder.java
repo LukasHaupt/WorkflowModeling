52313bec32e9f9168205149f3a219d1efacc1106
AlexandruTop <top.alexandru.90@gmail.com>
2014-05-20 11:43:17 +0200

    commit test

--- src/main/java/group1/foodsupply/CruiseRouteBuilder.java
+++ src/main/java/group1/foodsupply/CruiseRouteBuilder.java
@@ -8,7 +8,7 @@ public class CruiseRouteBuilder extends RouteBuilder {
 	public static void main(String[] args) throws Exception {
         new Main().run(args);
     }
-	
+	//test!
     public void configure() {
     	System.out.println("configuration");
         //from("file:src/data?noop=true").bean(new Test()).to("file:src/target");
